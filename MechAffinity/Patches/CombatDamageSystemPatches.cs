f1e357e09dfaab7a3efebe149cbf1d6cc5557161
MaxWasUnavailable <89798523+MaxWasUnavailable@users.noreply.github.com>
2024-06-05 08:42:22 +0200

    Apply damage reduction

---
+++ MechAffinity/Patches/CombatDamageSystemPatches.cs
@@ -0,0 +1,24 @@
+using HarmonyLib;
+using MechAffinity.Features;
+using PhantomBrigade.Combat.Systems;
+
+namespace MechAffinity.Patches;
+
+[HarmonyPatch(typeof(CombatDamageSystem))]
+internal static class CombatDamageSystemPatches
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(CombatDamageSystem.OnPartDamage))]
+    private static void OnPartDamagePrefix(CombatDamageSystem __instance,
+        ref PersistentEntity unitPersistentDamaged,
+        ref CombatEntity unitCombatDamaged,
+        ref EquipmentEntity partDamaged,
+        ref CombatEntity damageEvent,
+        ref float damage,
+        ref string directionKey,
+        ref bool normalized,
+        ref bool blockLoss)
+    {
+        damage *= MechAffinityBonus.GetDamageReductionMultiplier(unitPersistentDamaged);
+    }
+}
